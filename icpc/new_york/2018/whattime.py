928b5311d4e1ef2e9114769808b1d1feaab00600
Yannick KIKI <seyive.kiki@gmail.com>
2019-10-08 20:53:54 +0100

    Refactor whattime code

--- icpc/new_york/2018/whattime.py
+++ icpc/new_york/2018/whattime.py
@@ -24,29 +24,31 @@ with open('e.in', 'r') as f:
             if h[0]=='-': m = -int(m)
             offsets.append({'h': int(h), 'm': int(m)})
             
-        init_times = [get_real_time(times[0], offset) for offset in offsets]
-        offsets_idxs = defaultdict(list)
-        for idx, time in enumerate(init_times):
-            offsets_idxs[time].append((0, idx))
+        eventual_times = [get_real_time(times[0], offset) for offset in offsets]
+        matches = defaultdict(list) #real time -> (time_idx,offset_idx)
+        for offset_idx, time in enumerate(eventual_times):
+            matches[time].append((0, offset_idx))
         
-        for ti, time in enumerate(times[1:]):
+        for time_idx, time in enumerate(times[1:]):
             for idx, offset in enumerate(offsets):
                 real_time = get_real_time(time, offset)
-                time_idxs = offsets_idxs.get(real_time)
+                time_idxs = matches.get(real_time)
                 # if ti==2:
                     # print(time, offset, real_time)
                 if time_idxs:
-                    time_idxs.append((1+ti, idx))
+                    time_idxs.append((1+time_idx, idx))
         
-        # print(offsets_idxs)
+        # print(matches)
         results = list()
-        for time, idxs in offsets_idxs.items():
-            if len(list(set([var[0] for var in idxs])))==len(list(set([var[1] for var in idxs])))==n:
+        for time, match in matches.items():
+            if len(list(set([var[0] for var in match])))==len(list(set([var[1] for var in match])))==n:
                 results.append(time)
                 
-        display = 'none'
         n_results = len(results)
-        if n_results==1:
+        display = ''
+        if n_results==0:
+            display = 'none'
+        elif n_results==1:
             display = results[0]
         elif n_results>1:
             display = n_results
